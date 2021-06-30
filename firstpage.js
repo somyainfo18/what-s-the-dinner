23df0a150dfffe0513ea41624c26c1f89ed75dc8
Somya Nagaria <somyainfo18@gmail.com>
2021-06-30 12:47:06 -0500

    second commit

--- firstpage.js
+++ firstpage.js
@@ -1,23 +1,20 @@
-const navEl =document.querySelector("nav");
-const navTriggerEl =document.querySelector(".hamburger")
-const hamburgerBarsEl=document.getElementsByTagName("span");
-const content =document.querySelector(".content");
-
-
-
+const navTriggerEl = document.querySelector(".hamburger");
+const navEl = document.querySelector("nav");
+const contentEl = document.querySelector(".content");
+const hamburberBarsEl = document.getElementsByTagName("span");
 function toggleNav(){
-    navTriggerEl.addEventListener("click", function(){
-        navEl.classList.toggle("open");
-        contentEl.classList.toggle("shift");
-        animateHamburgers();
-        
-    })
+   navTriggerEl.addEventListener("click", function(){
+       navEl.classList.toggle("open");
+       contentEl.classList.toggle("shift");
+       animateHamburgers();
+       
+   })
 }
 
 function animateHamburgers(){
-    for (let item of hamburberBarsEl){
-       item.classList.toggle("change");
-    }
+   for (let item of hamburberBarsEl){
+      item.classList.toggle("change");
+   }
 }
 
 toggleNav();
